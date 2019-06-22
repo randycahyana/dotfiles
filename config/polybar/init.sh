644e3af3d0990c84c7383c2def7a3cd11b601695
Randy Cahyana <ranren.cahyana@gmail.com>
2019-06-22 11:32:37 +0700

    config: extend display to the right by default

--- config/polybar/init.sh
+++ config/polybar/init.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
+# extend second monitor to the right
+mons -e right
+
+# terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
+# wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
+# launch polybar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload top &
