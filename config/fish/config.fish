9424376a3bd55a521730b433d39d4f5b0b845956
Randy Cahyana <ranren.cahyana@gmail.com>
2019-10-28 00:18:33 +0700

    fish: initial config

---
+++ config/fish/config.fish
@@ -0,0 +1,44 @@
+# Go 
+set -gx GOROOT /home/$USER/go
+set -gx GOROOT_BOOTSTRAP /usr/local/go1.9.5
+set -gx GOPATH /home/$USER/workspace
+
+set -gx GOPRIVATE 'brank.as/*'
+set -gx GONOPROXY 'brank.as/*'
+set -gx GONOSUMDB 'brank.as/*'
+
+# PATH
+set -gx PATH $GOROOT/bin $PATH
+set -gx PATH $GOPATH/bin $PATH
+set -gx PATH /home/$USER/google-cloud-sdk/bin $PATH
+
+# Aliases
+
+alias d="kitty +kitten diff"
+alias dc="docker-compose"
+alias dot="cd $HOME/dotfiles"
+
+## kubectl
+alias kc="kubectl"
+function kcc
+	if test (count $argv) -eq 0
+		kc config get-contexts
+	else
+		kc config use-context $argv
+	end
+end
+
+## gcloud
+alias gcl="gcloud"
+function gcc
+	if test (count $argv) -eq 0
+		set -gx KUBECONFIG /home/$USER/.kube/config-personal
+		gcl config configurations activate rnd
+	else
+		set -gx KUBECONFIG /home/$USER/.kube/config-$argv
+		gcl config configurations activate $argv
+	end
+end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f "/home/$USER/google-cloud-sdk/path.fish.inc" ]; . "/home/$USER/google-cloud-sdk/path.fish.inc"; end
